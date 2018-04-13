93a96c07792de4fcce4ef00ef045e2fd253e2307
Sergio Puczi <sergio.tuas@hotmail.it>
2018-04-13 12:58:29 +0200

    Convert to MVC design pattern.

--- 17-Todoey/Controllers/TodoListViewController.swift
+++ 17-Todoey/Controllers/TodoListViewController.swift
@@ -12,16 +12,28 @@ class TodoListViewController: UITableViewController {
     
     let defaults = UserDefaults.standard
 
-    var itemArray = ["Pippo", "Pluto", "Paperino"]
+    var itemArray = [Item]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        //if Persistend Data Local Storage is not empty
-        if let item = defaults.array(forKey: "TodoListArray") as? [String] {
-            
-            //itemArray contains the data of Persistend Data Local Storage (TodoListArray)
+        let newItem = Item()
+        newItem.title = "Milk"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Bread"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Sugar"
+        itemArray.append(newItem3)
+        
+        //if Persistent Data Local Storage is not empty
+        if let item = defaults.array(forKey: "TodoListArray") as? [Item] {
+
+            //itemArray contains the data of Persistent Data Local Storage (TodoListArray)
             itemArray = item
         }
     }
@@ -30,8 +42,19 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
+        let item = itemArray[indexPath.row]
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        }
+//        else {
+//            cell.accessoryType = .none
+//        }
+        //Shorter form: value = condition ? valueIfTrue : valueIfFalse
+        cell.accessoryType = item.done ? .checkmark : .none
+
         
         return cell
     }
@@ -43,19 +66,15 @@ class TodoListViewController: UITableViewController {
     //MARK: - Tableview Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row])
         
         // Quando seleziono una cella non rimane sempre selezionata, ma si deseleziona subito dopo
         tableView.deselectRow(at: indexPath, animated: true)
         
-        // Se c'è già la spunta (checkmark) e seleziono di nuovo la cella -> toglila
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else {
-            // Altrimenti -> mettila
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        //sets the "done" property to it's opposite
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        //call Tableview Datasource Methods again
+        tableView.reloadData()
         
     }
     
@@ -67,10 +86,12 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user click the "Add Item" button on our UIAlert
+            let newItem = Item()
+            newItem.title = textField.text!
 
-            self.itemArray.append(textField.text!)
+            self.itemArray.append(newItem)
             
-            //save
+            //save data on "TodoListArray" Persistent Data Local Storage
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
---
+++ 17-Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  17-Todoey
+//
+//  Created by Sergio Puczi on 13/04/18.
+//  Copyright © 2018 Sergio Puczi. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
